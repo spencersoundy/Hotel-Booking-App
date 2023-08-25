7ff345ef4d006cdd63d0043c2fafe559f8e0344d
Spencer Soundy <112649118+spencersoundy@users.noreply.github.com>
2023-08-25 12:17:29 +0200

    facility
    backend
    front end

--- .history/admin/inc/header_20230825102448.php
+++ .history/admin/inc/header_20230825102448.php
@@ -15,9 +15,7 @@
                         <li class="nav-item">
                             <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a>
-                        </li>
+                        
                         <li class="nav-item">
                             <a class="nav-link text-white" href="user_queries.php">User Queries</a>
                         </li>
--- .history/admin/inc/header_20230825102543.php
+++ .history/admin/inc/header_20230825102543.php
@@ -16,7 +16,7 @@
                             <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="features_facilities.php">Features & Facilities</a>
+                            <a class="nav-link text-white" href="featurs.php">Features & Facilities</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="user_queries.php">User Queries</a>
