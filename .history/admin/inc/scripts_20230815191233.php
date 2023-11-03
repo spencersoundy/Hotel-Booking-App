28f86d7f54b68ca99c053608b8c0261851113a9c
Spencer Soundy <112649118+spencersoundy@users.noreply.github.com>
2023-11-03 13:03:32 +0200

    rooms front end
    rooms details page
    debugging

--- .history/admin/inc/scripts_20230815191233.php
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-<script>
-    function alert(type,msg){
-        let bs_class = (type= =='success') ? 'alert-success' : 'alert-danger';
-        let element = doc
-    }
-</script>
---
+++ .history/room_details_20231103094834.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soundy Hotels - ROOM DETAILS</title>
+    <?php require('inc/links.php') ?>
+</head>
+<body class="bg-light">
+   
+    <?php require('inc/header.php') ?>
+
+    <?php
+    if(!isset($_GET['id'])){
+        redirect('rooms.php');
+    }
+
+    $data = filteration($_GET);
+
+    $room_res = select("SELECT * FROM `rooms` WHERE `id`=? AND `status`=? AND `removed`=?",[$data['id'],1,0],'iii');
+
+    if(mysqli_num_rows($room_res)==0){
+        redirect('rooms.php');
+    }
+
+    $room_data = mysqli_fetch_assoc($room_res);
+    ?>
+
+
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 my-5 mb-4 px-4">
+                <h2 class="fw-bold"><?php echo $room_data['name'] ?></h2>
+                <div style="font-size: 14px;">
+                    <a href="index.php" class="text-secondary text-decoration-none">HOME</a>
+                    <span class="text-secondary "> > </span>
+                    <a href="rooms.php" class="text-secondary text-decoration-none">ROOMS</a>
+                </div>
+            </div>
+
+            <div class="col-lg-7 clo-md-12 px-4">
+                <div id="roomCarousel" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <p
+                        <div class="carousel-item active">
+                            <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#roomCarousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#roomCarousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
+
+        <div class="col-lg-9 co-md-12 px-4">
+
+            <?php
+                // $room_res = select("SELECT * FROM `rooms` WHERE `status`=? AND `removed`=?",[1,0],'ii');
+
+                // while($room_data = mysqli_fetch_assoc($room_res))
+                // {
+                //     // get features of room
+
+                //     $fea_q = mysqli_query($con,"SELECT f.name FROM `features` f 
+                //         INNER JOIN `room_features` rfea ON f.id = rfea.features_id 
+                //         WHERE rfea.room_id = '$room_data[id]'");
+
+                //     $features_data = "";
+                //     while($fea_row = mysqli_fetch_assoc($fea_q)){
+                //         $features_data .=" <span class='badge rounded-pill bg-light text-dark text-wrap'>
+                //             $fea_row[name]
+                //         </span>";
+                //     }
+
+                //     // get facilities of room
+
+                //     $fac_q = mysqli_query($con,"SELECT f.name FROM `facilities` f 
+                //         INNER JOIN `rooms_facilities` rfac ON f.id = rfac.facilities_id 
+                //         WHERE rfac.room_id = '$room_data[id]'");
+
+                //     $facilities_data = "";
+                //     while($fac_row = mysqli_fetch_assoc($fac_q)){
+                //         $facilities_data .=" <span class='badge rounded-pill bg-light text-dark text-wrap'>
+                //             $fac_row[name]
+                //         </span>";
+                //     }
+
+                //     // get thumbnail of image
+
+                //     $room_thumb = ROOMS_IMG_PATH."thumbnail.jpg";
+                //     $thumb_q = mysqli_query($con,"SELECT * FROM `room_images` 
+                //         WHERE `room_id`='$room_data[id]' 
+                //         AND `thumb`='1'");
+
+                //     if(mysqli_num_rows($thumb_q)>0){
+                //         $thumb_res = mysqli_fetch_assoc($thumb_q);
+                //         $room_thumb = ROOMS_IMG_PATH.$thumb_res['image'];
+                //     }
+
+                //     // print room card
+
+                //     echo <<< data
+                //         <div class="card mb-4 border-0 shadow">
+                //             <div class="row g-0 p-3 align-items-center">
+                //                 <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+                //                 <img src="$room_thumb" class="img-fluid rounded">
+                //             </div>
+                //                 <div class="col-md-5 px-lg-3 px-md-3 px-0">
+                //                     <h5 class="mb-3">$room_data[name]</h5>
+                //                     <div class="features mb-3">
+                //                         <h6 class="mb-1">Features</h6>
+                //                         $features_data
+                //                     </div>
+                //                     <div class="facilities mb-3">
+                //                         <h6 class="mb-1">Facilities</h6>
+                //                         $facilities_data
+                //                     </div>
+                //                     <div class="guests">
+                //                         <h6 class="mb-1">Guests</h6>
+                //                         <span class="badge rounded-pill bg-light text-dark text-wrap">
+                //                             $room_data[adult] Adults
+                //                         </span>
+                //                         <span class="badge rounded-pill bg-light text-dark text-wrap">
+                //                         $room_data[children] Children
+                //                         </span>
+                //                     </div>
+                //                 </div>
+                //                 <div class="col-md-2 mt-lg-0 mt-md-0 mt-4 text-center">
+                //                  <h6 class="mb-4">R$room_data[price] per night</h6>
+                //                  <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
+                //                   <a href="room_details.php?id=$room_data[id]" class="btn btn-sm w-100 btn-outline-dark shadow-none">More Details</a>
+                //                 </div>
+                //             </div>
+                //         </div>
+                //     data;
+                // }
+            ?>
+
+        </div>
+
+        </div>
+    </div>
+
+<?php require('inc/footer.php')?>
+
+</body>
+</html>
+
